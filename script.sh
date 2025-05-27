a26397ac669190a1f8a1561e198f21e202c896d5
Om Goyal <omgoyal2711@gmail.com>
2025-05-27 17:17:57 +0530

    Fix script to remove existing container before running a new instance

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 sudo docker build -t file-hoster . 
 
-sudo docker run -d -p 6200:6200 -v /home/om/DockerContainers/SambaShare/file-hosting-uploads:/app/uploaded-files --name file-hoster file-hoster
+sudo docker rm -f file-hoster 2>/dev/null || true && sudo docker run -d -p 6200:6200 -v /home/om/DockerContainers/SambaShare/file-hosting-uploads:/app/uploaded-files --name file-hoster file-hoster
 
 sudo docker update --restart unless-stopped file-hoster
